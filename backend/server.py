813e28e92f615a3f323a80e02cf2aca3cb54ff24
Jaiiiyu23 <jmakwana76@gmail.com>
2026-03-14 07:19:43 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
-from data_collector import get_latest_scams
 
 app = FastAPI()
 
-@app.get("/scams")
-def scams():
-    data = get_latest_scams()
-
-    return {
-        "latest_scams": data,
-        "count": len(data)
-    }
+@app.get("/")
+def home():
+    return {"message": "Fraud Intel Backend Running"}
